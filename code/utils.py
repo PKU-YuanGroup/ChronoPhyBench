7e2cf325d66111ba5b11574298b39522df56ed9c
KoHsin-pku25 <2501212818@stu.pku.edu.cn>
2026-06-18 16:46:13 +0800

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -1,1 +1,19 @@
+import re
+    
+def extract_answer(response):
+    match_conclusion = re.search(r'Conclusion[:\s]*(\d{4})', response, re.IGNORECASE)
+    if match_conclusion:
+        return match_conclusion.group(1)
+    
+    all_numbers = re.findall(r'\d{4}', response)
+    if all_numbers:
+        return all_numbers[-1]
+    
+    clean_digits = re.sub(r'\D', '', response)
+    if len(clean_digits) == 4:
+        return clean_digits
+        
+    return ""
+
+
 
